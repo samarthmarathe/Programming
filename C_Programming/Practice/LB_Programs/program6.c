2f3b01f131382440113da5d0e5a6c9ce983a22ae
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-07-29 15:23:18 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program6.c
@@ -0,0 +1,28 @@
+/*
+    Algorithm
+    START
+        Accept First number as No1
+        Accept Second number as No2
+        Perform Adiition of No1 and No2
+        Display the result
+    STOP
+*/
+#include<stdio.h>
+
+int main()
+{
+    // Variable creation with default values
+    float i = 0.0f, j = 0.0f, k = 0.0f;
+
+    printf("Enter First Number: \n");
+    scanf("%f",&i);
+
+    printf("Enter Second Number: \n");
+    scanf("%f",&j);
+
+    k = i +j;
+
+    printf("Additon is : %f\n",k);
+
+    return 0;
+}
