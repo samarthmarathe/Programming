53009583e09f2c538b355ed4f0d6903fd47f2682
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-08-23 23:59:12 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program100.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+    int Arr[5] = {0};
+
+    printf("Enter the Elements: \n");
+    scanf("%d",&Arr[0]);
+    scanf("%d",&Arr[1]);
+    scanf("%d",&Arr[2]);
+    scanf("%d",&Arr[3]);
+    scanf("%d",&Arr[4]);
+    
+    printf("Elements of the Array are: \n");
+    
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+
+    return 0;
+}
