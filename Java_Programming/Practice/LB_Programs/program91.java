df917135436597de68c5afa757fbd3853472db5a
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-08-23 23:53:42 +0530

    Add files via upload

---
+++ Java_Programming/Practice/LB_Programs/program91.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class program91
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+        DigitX dobj = new DigitX();
+
+        int iValue = 0;
+        int iRet = 0;
+
+        System.out.println("Enter Number: ");
+        iValue = sobj.nextInt();
+
+        iRet = dobj.SumDigits(iValue);
+
+        System.out.println("Addition of Digits are: "+iRet);
+    }
+}
+
+class DigitX
+{
+    public int SumDigits(int iNo)
+    {
+        int iSum = 0;
+
+        while(iNo != 0)
+        {            
+            iSum = iSum + (iNo % 10);
+
+            iNo =iNo/10;
+        }
+
+        return iSum;
+    }
+}
