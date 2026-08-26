6748e3bbad7da26d8d9f1a71c8eb1b82fcf1fa73
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-08-26 16:20:49 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program123.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Summation(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        iSum = iSum + Arr[iCnt];
+    }
+
+    return iSum;
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0;
+    int iCnt = 0;
+    int iRet = 0;
+
+    printf("Enter number of elements: \n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter the elemnts: \n");
+    
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = Summation(Brr,iLength);
+
+    printf("Addition is: %d\n",iRet);
+
+    free(Brr); 
+
+    return 0;
+}
