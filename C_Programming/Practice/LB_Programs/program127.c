21a30eb41728b57a6e0230147dab7ec2e6c545c5
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-08-26 16:22:20 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program127.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int CountFrequency(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iCount = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == 11)
+        {
+            iCount++;
+        }
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0;
+    int iCnt = 0;
+    int iRet = 0;
+
+    printf("Enter number of elements: \n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter the elemnts: \n");
+    
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = CountFrequency(Brr,iLength);
+
+    printf("Frequency of elements is: %d\n",iRet);
+
+    free(Brr); 
+
+    return 0;
+}
---
+++ C_Programming/Practice/LB_Programs/program128.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+bool LinearSearch(int Arr[], int iSize)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == 11)
+        {
+            return true;        //Bad Programming
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0;
+    int iCnt = 0;
+    bool bRet = false;
+
+    printf("Enter number of elements: \n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter the elemnts: \n");
+    
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    bRet = LinearSearch(Brr,iLength);
+
+    if(bRet == true)
+    {
+        printf("Element is present\n");
+    }
+    else
+    {
+        printf("Element is not present\n");
+    }
+
+    free(Brr); 
+
+    return 0;
+}
