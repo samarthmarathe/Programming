33ae18b8f3d170463862903c4b9dc0cb87d5c37d
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-08-26 16:21:56 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program126.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int CountOdd(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iCount = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] % 2 == 1)
+        {
+            iCount++;
+        }
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0;
+    int iCnt = 0;
+    int iRet = 0;
+
+    printf("Enter number of elements: \n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter the elemnts: \n");
+    
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = CountOdd(Brr,iLength);
+
+    printf("Odd elements are: %d\n",iRet);
+
+    free(Brr); 
+
+    return 0;
+}
