4e2883ce10edf06704354ae28211feadc0c04d5f
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-08-27 21:06:56 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program135.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Time Complexity o(N)
+
+int Maximum(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iMax = 0;
+
+    iMax = Arr[0];
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] > iMax)
+        {
+            iMax = Arr[iCnt];
+        }
+    }
+    return iMax;
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0;
+    int iCnt = 0;
+    int iRet = 0;
+
+    printf("Enter number of elements: \n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter the elemnts: \n");
+    
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = Maximum(Brr,iLength);
+
+    printf("Maximum element is %d",iRet);
+
+    free(Brr); 
+
+    return 0;
+}
