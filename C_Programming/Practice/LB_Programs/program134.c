8b4f40c8b6911ea43082fe906fe3beddf16e49fb
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-08-27 21:06:15 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program134.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+bool LinearSearch(int Arr[], int iSize, int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            break;
+        }
+    }
+
+    if(iCnt < iSize)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0;
+    int iCnt = 0;
+    bool bRet = false;
+    int iValue = 0;
+
+    printf("Enter number of elements: \n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter the elemnts: \n");
+    
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    printf("Enter the element that you want to search: \n");
+    scanf("%d",&iValue);
+
+    bRet = LinearSearch(Brr,iLength,iValue);
+
+    if(bRet == true)
+    {
+        printf("Element is present\n");
+    }
+    else
+    {
+        printf("Element is not present\n");
+    }
+
+    free(Brr); 
+
+    return 0;
+}
