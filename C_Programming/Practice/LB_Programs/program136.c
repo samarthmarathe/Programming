928806a6d557a799912a213ec461c1e65f87be2b
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-08-27 21:07:19 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program136.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Time Complexity o(N)
+
+int Minimum(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iMin = 0;
+
+    iMin = Arr[0];
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] < iMin)
+        {
+            iMin = Arr[iCnt];
+        }
+    }
+    return iMin;
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0;
+    int iCnt = 0;
+    int iRet = 0;
+
+    printf("Enter number of elements: \n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter the elemnts: \n");
+    
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = Minimum(Brr,iLength);
+
+    printf("Minimum element is %d",iRet);
+
+    free(Brr); 
+
+    return 0;
+}
