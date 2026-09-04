958004b56cd89f14cc10810d82269c69565b56ae
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-09-04 21:50:55 +0530

    Add files via upload

---
+++ C++_Programming/Practice/LB_Programs/program159.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class ArrayX
+{
+    public:
+        int *Arr;
+        int iSize;
+
+        //Parametrised constructor
+        ArrayX(int X)
+        {
+            cout<<"Inside Constructor\n";
+            iSize = X;                  //Characteristics initialisation
+            Arr = new int[iSize];       //Resourse Allocation
+        }
+
+        //Destructor
+        ~ArrayX()
+        {
+            cout<<"Inside Destructor\n";
+            delete []Arr;               //Resourse Dealloation
+        }
+};
+
+int main()
+{
+    // Static Memory allocation for object 
+    // ArrayX aobj1(5);
+
+    ArrayX *aobj1 = new ArrayX(5);   
+
+    return 0;
+}
