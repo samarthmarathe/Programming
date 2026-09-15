ab0b65a7aed1a4aa26cfe2c5e8034b4011715715
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-09-15 22:23:51 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program243.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void Display(char str[])
+{
+    printf("Input string is: %s\n",str);
+}
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter string: \n");
+    scanf("%[^'\n']s",Arr);             // %[^'\n']s this is regex
+
+    Display(Arr);
+    return 0;
+}
