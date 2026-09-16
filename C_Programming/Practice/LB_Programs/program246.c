981757b64dc1433a4f5cb321b763400a44dfa3d2
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-09-16 23:48:09 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program246.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int strlenX(char *str)
+{
+    int iCount = 0;
+
+    while(*str != '\0')
+    {
+        iCount++;
+        str++;
+    }
+    return iCount;
+}
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter string: \n");
+    scanf("%[^'\n']s",Arr);             // %[^'\n']s this is regex
+
+    iRet = strlenX(Arr);
+    
+    printf("String length is: %d\n",iRet);
+
+    return 0;
+}
