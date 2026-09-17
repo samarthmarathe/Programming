ab9006e34e184975ab193c831088189adf144135
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-09-17 23:42:39 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program254.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int CountSmall(const char * str)
+{
+    int iCount = 0;
+
+    while(*str != '\0')
+    {
+        if(*str >= 97 && *str <= 122)
+        {
+            iCount++;
+        }
+        
+        str++;
+    }
+    return iCount;
+}
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter string: \n");
+    scanf("%[^'\n']s",Arr);             // %[^'\n']s this is regex
+
+    iRet = CountSmall(Arr);
+    
+    printf("Frequency of Small alphabets are: %d\n",iRet);
+
+    return 0;
+}
