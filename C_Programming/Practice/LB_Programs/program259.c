d8f6e8d1d105a6bbc9fc981c15682ecd9e238280
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-09-18 20:26:58 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program259.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void Update(char * str)
+{
+    int iCount = 0;
+
+    while(*str != '\0')
+    {
+        if(*str == 'l')
+        {
+            *str = 'L';
+        }
+        
+        str++;
+    }
+}
+int main()
+{
+    char Arr[50] = {'\0'};
+    
+    printf("Enter string: \n");
+    scanf("%[^'\n']s",Arr);             // %[^'\n']s this is regex
+
+    Update(Arr);
+    
+    printf("Updated String: %s\n",Arr);
+
+    return 0;
+}
