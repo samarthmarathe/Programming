9f4e6202aa349aaa9f7f55913e15f5c9527e9e93
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-09-21 20:32:12 +0530

    Add files via upload

---
+++ Java_Programming/Practice/LB_Programs/program281.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class StringX
+{
+    public String toUpperX(String str)
+    {
+        int i = 0;
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            Arr[i] = Arr[i] - 32;           // error
+        }
+
+        return new String(Arr);
+    }
+}
+
+class program281
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        String data = null;
+
+        System.out.println("Enter string: ");
+        data = sobj.nextLine();
+        
+        StringX strobj = new StringX();
+
+        String sRet = null;
+
+        sRet = strobj.toUpperX(data);
+
+        System.out.println("Updated String is: "+sRet);
+    }    
+}
