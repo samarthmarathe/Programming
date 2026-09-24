f38669377c4d57e5db8d52082484012ae53bc024
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-09-24 18:24:41 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program295.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void strrevX(char *str)
+{
+    char *start = NULL;
+    char *end = NULL;
+    start = str;
+
+    while(*str != '\0')
+    {
+        str++;
+    }
+
+        str--;
+
+    while(str >= start)
+    {
+        printf("%s\n",str);
+        str--;
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter string: \n");
+    scanf("%[^'\n']s",Arr);
+
+    ReverseDisplay(Arr);
+
+    return 0;
+}
