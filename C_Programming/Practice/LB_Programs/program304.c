04cfd4b912889e7f706aad1c76a1c3b2f330a261
Samarth Marathe <samarthmarathe2007@gmail.com>
2026-09-25 13:36:37 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Programs/program304.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int No1 = 0, No2 = 0, Ans = 0;
+
+    printf("Enter First number: \n");
+    scanf("%d",&No1);
+
+    printf("Enter Second number: \n");
+    scanf("%d",&No2);
+
+    Ans = No1 & No2;
+
+    printf("Result is: %d\n",Ans);
+
+    return 0;
+}
